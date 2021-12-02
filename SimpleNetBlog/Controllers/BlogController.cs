5f057d1c59f3d740234652daea33359547f96230
Marek Czarnecki <marek.czarnecki@gds.ey.com>
2021-12-02 15:40:33 +0100

    sample MD page rendered as HTML

---
+++ SimpleNetBlog/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleNetBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string fileTitle)
+        {
+            var mdPath = Path.Combine("./BlogData/", fileTitle +".md");
+            //var result = Markdown.ToHtml("This is a text with some *emphasis*");
+            string readText = System.IO.File.ReadAllText(mdPath);
+            var result = Markdown.ToHtml(readText);
+
+            ViewBag.MDHtml = result;
+            return View();
+        }
+    }
+}
---
+++ SimpleNetBlog/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Mvc;
+using SimpleNetBlog.Models;
+using System.Diagnostics;
+
+namespace SimpleNetBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DynamicBlogPost(string fileName)
+        {
+            var mdPath = Path.Combine("./BlogData/", fileName);
+            //var result = Markdown.ToHtml("This is a text with some *emphasis*");
+            string readText = System.IO.File.ReadAllText(mdPath);
+            var result = Markdown.ToHtml(readText);
+
+            ViewBag.MDHtml = result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
